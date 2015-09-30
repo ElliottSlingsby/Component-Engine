9495076d11c37ae8f208762e69bad77016b97e64
Elliott Slingsby <elliottslingsby@googlemail.com>
2015-09-30 08:47:16 +0100

    Larger default draw distance

--- engine/Component/Camera.hpp
+++ engine/Component/Camera.hpp
@@ -12,7 +12,7 @@ class Camera : public HelperComponent{
 	float _fogDensity = 0.f;
 
 	unsigned int _fov = 90;
-	unsigned int _drawDistance = 2048;
+	unsigned int _drawDistance = 4096;
 
 public:
 	void load();
